788bf75afc6b54f395772c224e24696ad43f7f5d
GMdianaty <GMdianaty@gmail.com>
2014-09-28 22:38:56 +0000

    Update!

---
+++ MAC/v13/Step2.sh
@@ -0,0 +1,4 @@
+echo ==[HangoutsLogPlus Mac]==
+echo Welcome back! You can host a hangout and replicate your problem
+echo Then E-Mail The Logs by Executing: OpenLogs.sh!
+echo Also, Execute DisableHangoutsLog.sh to Disable logging.
